1a46fde23f8b797a34b1d444afe2f7c6ff292cb3
alokkuri <alokkuri98@gmail.com>
2026-02-28 20:04:58 +0600

    Abstract class & methods

---
+++ Abstract_class/main.py
@@ -0,0 +1,54 @@
+# Abstract class = A class that can not be instantiated on its own; Meant to be subclassed.
+#                  They can contain abstract method, which are decleared but have no implementation.
+#                  Abstract classed benifits
+#                  1. Prevents instantiation of the class itself
+#                  2. Requires children to used inherited abstract method
+
+from abc import ABC, abstractmethod
+
+
+class Vehicle(ABC):
+
+    @abstractmethod
+    def go(self):
+        pass
+
+    @abstractmethod
+    def stop(self):
+        pass
+
+
+class Car(Vehicle):
+    def go(self):
+        print("You drive the car")
+
+    def stop(self):
+        print("You stop the car")
+
+
+class Motorcycle(Vehicle):
+
+    def go(self):
+        print("You ride the motorcycle")
+
+
+    def stop(self):
+        print("You stop the motorcycle")
+
+class Boat(Vehicle):
+    def go(self):
+        print("You sail the boat")
+
+    def stop(self):
+        print("You anchor the boad")
+
+
+
+# vehicle = Vehicle()
+                              # error = Can't instantiate abstract class Vehicle without an implementation for abstract methods 'go', 'stop'
+car = Car()                   # When inherit from abstruct method must be implement abstract method also. Otherwise error
+motorcycle = Motorcycle()
+boat = Boat()
+
+boat.go()
+boat.stop()
